e8437431614710bb5129cad4e86ed492e0612209
metinakkin <metin_akkin@hotmail.com>
2017-01-02 12:44:55 +0200

    Add files via upload

---
+++ Prototip_Robot_El/Prototip_Robot_El.ino
@@ -0,0 +1,27 @@
+#include<Servo.h>
+Servo servo1;
+const int flexpin = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  servo1.attach(9);
+
+}
+
+void loop()
+{
+  int flexposition;
+  int servoposition;
+  flexposition = analogRead(flexpin);
+  Serial.println(flexposition);
+  servoposition = map(flexposition, 0, 1023, 0, 180);
+  servoposition = constrain(flexposition, 0, 180);
+  servo1.write(servoposition);
+  Serial.print("sensor:");
+  Serial.print(flexposition);
+  Serial.print("servo:");
+  Serial.print(servoposition);
+  delay(200);
+
+}
